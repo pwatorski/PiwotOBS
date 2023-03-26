4db3bb3a776d4a62518d9a2bc08438a2df88125a
Piotr Wątorski <piotr.watorski@gmail.com>
2023-03-26 17:32:02 +0200

    add proper resizing

--- PiwotOBS/Structure/SceneItem.cs
+++ PiwotOBS/Structure/SceneItem.cs
@@ -14,30 +14,32 @@ namespace PiwotOBS.Structure
 
     public partial class SceneItem
     {
-        public Float2 OBSPosition 
-        { 
-            get=>Transform?.Position??Float2.Zero; 
-            protected set { 
-                if (Transform != null) 
-                { 
-                    Transform.positionX = value.X; 
-                    Transform.positionX = value.Y; 
-                } 
-            } 
+        public Float2 OBSPosition
+        {
+            get => Transform?.Position ?? Float2.Zero;
+            protected set
+            {
+                if (Transform != null)
+                {
+                    Transform.positionX = value.X;
+                    Transform.positionX = value.Y;
+                }
+            }
         }
-        public Float2 CurPosition { get => curPosition??OBSPosition; protected set { curPosition = value; } }
+        public Float2 CurPosition { get => curPosition ?? OBSPosition; protected set { curPosition = value; } }
         protected Float2? curPosition = null;
 
         public Float2 OBSScale
-        { 
-            get=>Transform?.Scale??Float2.Zero; 
-            protected set { 
-                if (Transform != null) 
-                { 
-                    Transform.scaleX = value.X; 
-                    Transform.scaleY = value.Y; 
+        {
+            get => Transform?.Scale ?? Float2.Zero;
+            protected set
+            {
+                if (Transform != null)
+                {
+                    Transform.scaleX = value.X;
+                    Transform.scaleY = value.Y;
                 }
-            } 
+            }
         }
         public Float2 CurScale { get => curScale ?? OBSScale; protected set { curScale = value; } }
         protected Float2? curScale = null;
@@ -55,7 +57,7 @@ namespace PiwotOBS.Structure
         }
         public Float2 CurSize { get => curSize ?? OBSSize; protected set { curSize = value; } }
         protected Float2? curSize = null;
-        public float OBSRotation { get=> Transform?.rotation??0; protected set { if (Transform != null) Transform.rotation=value; } }
+        public float OBSRotation { get => Transform?.rotation ?? 0; protected set { if (Transform != null) Transform.rotation = value; } }
         public float CurRotation { get; protected set; }
         public SceneItem? Parent { get; protected set; }
 
@@ -66,7 +68,7 @@ namespace PiwotOBS.Structure
             Parent = null;
         }
 
-        internal void SetParent(SceneItem parent) 
+        internal void SetParent(SceneItem parent)
         {
             Parent = parent;
             SceneName = GetSceneName();
@@ -84,8 +86,8 @@ namespace PiwotOBS.Structure
         {
             JsonObject transform = new JsonObject()
             {
-                { "height", CurSize.X},
-                { "width", CurSize.Y},
+                //{ "height", CurSize.Y},
+                //{ "width", CurSize.X},
                 { "positionX", CurPosition.X},
                 { "positionY", CurPosition.Y},
                 { "scaleX", CurScale.X},
@@ -97,17 +99,17 @@ namespace PiwotOBS.Structure
 
         protected string GetSceneName()
         {
-            if(Parent == null)
+            if (Parent == null)
             {
                 return Name;
             }
 
-            if(Parent?.IsScene??false)
+            if (Parent?.IsScene ?? false)
             {
                 return Parent.Name;
             }
 
-            return Parent?.GetSceneName()??string.Empty;
+            return Parent?.GetSceneName() ?? string.Empty;
         }
 
         public void TransformObject(
@@ -116,15 +118,15 @@ namespace PiwotOBS.Structure
             Float2? newScale = null,
             Float2? deltaScale = null,
             Float2? relativeScale = null,
-            float? newRotation=null,
-            float deltaRotation=0)
+            float? newRotation = null,
+            float deltaRotation = 0)
         {
-            if(deltaPos == null)
+            if (deltaPos == null)
             {
                 deltaPos = Float2.Zero;
             }
 
-            
+
 
             if (newScale == null)
             {
@@ -144,19 +146,39 @@ namespace PiwotOBS.Structure
 
             CurPosition = newPos ?? CurPosition + deltaPos;
             CurRotation = newRotation ?? CurRotation + deltaRotation;
-
+            CurSize = OBSSize * CurScale / OBSScale;
             OBSDeck.OBS.SetSceneItemTransform(SceneName, SceneItemId, GetCurrentTransformJson());
         }
 
 
-        public void ScaleObjectRelative(float relativeScaleX = 1, float relativeScaleY = 1)
+        public void SetRelativeScale(float relativeScaleX = 1, float relativeScaleY = 1)
         {
             TransformObject(relativeScale: new Float2(relativeScaleX, relativeScaleY));
+        }
+
+        public void SetRelativeScale(Float2 relativeScale)
+        {
+            TransformObject(relativeScale: relativeScale);
+        }
+
+        public void SetSize(float? sizeX = null, float? sizeY = null)
+        {
+            SetRelativeScale(new Float2(sizeX ?? OBSSize.X, sizeY ?? OBSSize.Y));
+        }
+
+        public void SetSize(Float2 size)
+        {
+            SetRelativeScale(size / OBSSize);
         }
 
         public void SetRelativePosition(float deltaX = 0, float deltaY = 0)
         {
             TransformObject(newPos: OBSPosition + new Float2(deltaX, deltaY));
+        }
+
+        public void SetRelativePosition(Float2 delta)
+        {
+            TransformObject(newPos: OBSPosition + delta);
         }
 
     }
