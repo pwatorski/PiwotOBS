abeac964351e828fc4a1d212e845a82274d00f2e
pwatorski <piotr.watorski@gmail.com>
2026-05-06 13:21:37 +0200

    aaa

--- PiwotOBS/OBSDeck.cs
+++ PiwotOBS/OBSDeck.cs
@@ -35,8 +35,8 @@ namespace PiwotOBS
             {
                 try
                 {
-                    OBS.ConnectAsync("ws://127.0.0.1:4455", "zaq1235pli");
-                    //OBS.ConnectAsync($"ws://{ip}:{port}", password);
+                    //OBS.ConnectAsync("ws://127.0.0.1:4455", "zaq1235pli");
+                    OBS.ConnectAsync($"ws://{ip}:{port}", password);
                 }
                 catch (Exception ex)
                 {
